6575b54a05478f218bb282f9b97ff63a2516665c
pratikcf <pratik@cloudfactory.com>
2016-03-30 14:12:51 +0545

    Spaces add.

--- each.rb
+++ each.rb
@@ -4,7 +4,7 @@ array.each do |name|
 end
 
 puts "using array.each"
-array.each {|name| puts "#{name} is my friend."}
+array.each { |name| puts "#{name} is my friend." }
 
 puts "Using each.with_index"
-array.each.with_index(1){|name,index| puts "#{index}. #{name} is my friend"}
+array.each.with_index(1) { |name,index| puts "#{index}. #{name} is my friend" }
