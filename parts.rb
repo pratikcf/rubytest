5809559ecec818e27c1b2612261ecab7455ecb41
pratikcf <pratik@cloudfactory.com>
2016-03-31 15:29:08 +0545

    Multiple entries csv.

--- parts.rb
+++ parts.rb
@@ -1,6 +1,10 @@
 require 'csv'
 
 CSV.open("test_file.csv", "wb") do |csv|
-  csv << [ :Sno, :fname, :mname, :lname, :age, :dob ]
-  csv << ["1", "Neli", " adf", "adfadf", "23", "19362"]
+  csv << [ :Sno, :FirstName, :MName, :LastName, :Age, :DateofBirth ]
+  csv << ["1", "Neli", " adf", "gurung", "23", "1991-05-24"]
+  csv << ["2", "Pratik", " adf", "GH", "23", "1993-04-13"]
+  csv << ["3", "Bibek", " adf", "KD", "23", "1992-03-23"]
+  csv << ["4", "Puja", " adf", "JHa", "23", "1989-05-09"]
+  csv << ["5", "Batsal", " adf", "AWL", "23", "1994-09-23"]
 end
